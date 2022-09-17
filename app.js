0679f0732edce1ab5b34e12b27ed26a7ed07a312
Nina Christianson <ninaxianson@gmail.com>
2022-09-17 10:41:34 +1000

    Add function to call game

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-let playerScore = 0;
 let computerScore = 0;
-
+let playerScore = 0;
 
 let getComputerChoice = () => {
   const choices = ["rock", "paper", "scissors"];
@@ -8,7 +7,7 @@ let getComputerChoice = () => {
   return choices[random];
 };
 
-function playRound(playerSelection, computerSelection) { 
+function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     return `Computer chose ${computerSelection}, you chose ${playerSelection}. 
     It's a tie!`;
@@ -17,44 +16,36 @@ function playRound(playerSelection, computerSelection) {
     (computerSelection === "paper" && playerSelection === "rock") ||
     (computerSelection === "scissors" && playerSelection === "paper")
   ) {
-    return `Computer chose ${computerSelection}, you chose ${playerSelection}. 
-    The computer wins this round.`;
+    console.log(`Computer chose ${computerSelection}, you chose ${playerSelection}. 
+  The computer wins this round.`);
     computerScore++;
   } else if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "scissors" && computerSelection === "paper")
   ) {
-    return `Computer chose ${computerSelection}, you chose ${playerSelection}. 
-    You win this round.`;
-    playerScore++
+    console.log(`You chose ${playerSelection}, the computer chose ${computerSelection}. 
+  You win this round.`);
+    playerScore++;
   } else {
-    return "You have entered an invalid choice. Please enter rock, paper, or scissors"
-  }
-  
-}
-function endGame(playerScore, computerScore) {
-  if (playerScore === 5 || computerScore === 5) {
-    console.log("Game Over")
-  } if (playerScore > computerScore) {
-    return "You are the rock, paper, scissors champ!!!"
-  } else {
-    return "The computer won the game! Try your luck again."
+    return "You have entered an invalid choice. Please enter rock, paper, or scissors";
   }
 }
 
 function game() {
   for (let i = 0; i < 5; i++) {
-    playRound();
+    const playerSelection = prompt(
+      "Choose paper, rock, or scissors"
+    ).toLowerCase();
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
   }
-  endGame();
+  if (playerScore > computerScore) {
+    return "You've beaten the computer this game!!!";
+  } else if (computerScore > playerScore) {
+    return "The computer beat you this game. Try again!";
+  } else {
+    return "You tied the computer this game!";
+  }
 }
-
-
-
-
-const playerSelection = prompt("Choose paper, rock, or scissors").toLowerCase();
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection))
-
-game();
+console.log(game());

9d91674df0aee476e7e10103bd646632aefc8d03
Nina Christianson <ninaxianson@gmail.com>
2022-09-23 06:01:22 +1000

    Fix tied result

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const resultDisplay = document.querySelector(".result");
-const winner = document.querySelector(".winner");
-const loser = document.querySelector(".loser");
-const tied = document.querySelector(".tied");
+const winnerStatement = document.querySelector(".winner");
+const loserStatement = document.querySelector(".loser");
+const tiedStatement = document.querySelector(".tied");
 let pScore = 0;
 let cScore = 0;
 let result;
@@ -19,25 +19,37 @@ const updateScore = () => {
   computerScore.textContent = cScore;
 };
 
-function getGameWinner(pScore, cScore) {
+function getGameWinner() {
   if (pScore === 5 && cScore === 5) {
-    tied.style.display = "block";
+    winnerStatement.style.display = "hidden";
+    loserStatement.style.display = "hidden";
+    tiedStatement.style.display = "block";
   }
-  if (pScore === 5) {
-    winner.style.display = "block";
+  if (pScore === 5 && cScore < 5) {
+    winnerStatement.style.display = "block";
   }
-  if (cScore === 5) {
-    loser.style.display = "block";
+  if (cScore === 5 && pScore < 5) {
+    loserStatement.style.display = "block";
   }
 }
 
 function reset() {
-  if (pScore > 5 || cScore > 5) {
+  if (pScore >= 6 && cScore >= 6) {
     pScore = 0;
     cScore = 0;
-    loser.style.display = "none";
-    winner.style.display = "none";
-    tied.style.display = "none";
+    tiedStatement.style.display = "none";
+    winnerStatement.style.display = "none";
+    loserStatement.style.display = "none";
+    result = "";
+  } else if ((pScore >= 6 && cScore < 6) || (cScore >= 6 && pScore < 6)) {
+    pScore = 0;
+    cScore = 0;
+    winnerStatement.style.display = "none";
+    tiedStatement.style.display = "none";
+    loserStatement.style.display = "none";
+    result = "Play Again";
+  } else {
+    console.log("Error");
   }
 }
 
@@ -51,6 +63,7 @@ let playerChoices = document
       playRound(playerSelection, computerSelection);
       updateScore();
       getGameWinner(pScore, cScore);
+      reset;
       resultDisplay.innerText = result;
     });
   });

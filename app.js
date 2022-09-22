3e982d9bf4940c2857d052197131aa7b7f3cf1cc
Nina Christianson <ninaxianson@gmail.com>
2022-09-22 10:38:07 +1000

    update ui

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-let computerScore = 0;
-let playerScore = 0;
+const resultDisplay = document.querySelector(".result");
+const winner = document.querySelector(".winner");
+const loser = document.querySelector(".loser");
+const tied = document.querySelector(".tied");
+let pScore = 0;
+let cScore = 0;
+let result;
 
 let getComputerChoice = () => {
   const choices = ["rock", "paper", "scissors"];
@@ -7,45 +12,77 @@ let getComputerChoice = () => {
   return choices[random];
 };
 
+const updateScore = () => {
+  let computerScore = document.querySelector(".computer-score");
+  let playerScore = document.querySelector(".player-score");
+  playerScore.textContent = pScore;
+  computerScore.textContent = cScore;
+};
+
+function getGameWinner(pScore, cScore) {
+  if (pScore === 5 && cScore === 5) {
+    tied.style.display = "block";
+  }
+  if (pScore === 5) {
+    winner.style.display = "block";
+  }
+  if (cScore === 5) {
+    loser.style.display = "block";
+  }
+}
+
+function reset() {
+  if (pScore > 5 || cScore > 5) {
+    pScore = 0;
+    cScore = 0;
+    loser.style.display = "none";
+    winner.style.display = "none";
+    tied.style.display = "none";
+  }
+}
+
+let playerChoices = document
+  .querySelectorAll("button")
+  .forEach((possibleChoice) => {
+    possibleChoice.addEventListener("click", function () {
+      const playerSelection = possibleChoice.id;
+      const computerSelection = getComputerChoice();
+
+      playRound(playerSelection, computerSelection);
+      updateScore();
+      getGameWinner(pScore, cScore);
+      resultDisplay.innerText = result;
+    });
+  });
+
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    return `Computer chose ${computerSelection}, you chose ${playerSelection}. 
-    It's a tie!`;
+    result = `Computer chose ${computerSelection}, you chose ${playerSelection}. 
+    It's a tie! You each win a point!`;
+    pScore++;
+    cScore++;
+    reset();
   } else if (
     (computerSelection === "rock" && playerSelection === "scissors") ||
     (computerSelection === "paper" && playerSelection === "rock") ||
     (computerSelection === "scissors" && playerSelection === "paper")
   ) {
-    console.log(`Computer chose ${computerSelection}, you chose ${playerSelection}. 
-  The computer wins this round.`);
-    computerScore++;
+    result = `Computer chose ${computerSelection}, you chose ${playerSelection}. 
+  The computer wins this round.`;
+    cScore++;
+    reset();
   } else if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "scissors" && computerSelection === "paper")
   ) {
-    console.log(`You chose ${playerSelection}, the computer chose ${computerSelection}. 
-  You win this round.`);
-    playerScore++;
+    result = `You chose ${playerSelection}, the computer chose ${computerSelection}. 
+  You win this round.`;
+    pScore++;
+    reset();
   } else {
-    return "You have entered an invalid choice. Please enter rock, paper, or scissors";
+    result =
+      "You have entered an invalid choice. Please enter rock, paper, or scissors";
   }
+  resultDisplay.innerText = result;
 }
-
-function game() {
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt(
-      "Choose paper, rock, or scissors"
-    ).toLowerCase();
-    const computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
-  }
-  if (playerScore > computerScore) {
-    return "You've beaten the computer this game!!!";
-  } else if (computerScore > playerScore) {
-    return "The computer beat you this game. Try again!";
-  } else {
-    return "You tied the computer this game!";
-  }
-}
-console.log(game());
